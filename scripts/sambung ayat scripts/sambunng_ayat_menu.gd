84b51d6a166b40d235e683a218eb8af5f330d09a
LAB PPLG <prama0793@gmail.com>
2025-04-10 07:32:54 +0700

    Ganti notification, background dll

--- scripts/sambung ayat scripts/sambunng_ayat_menu.gd
+++ scripts/sambung ayat scripts/sambunng_ayat_menu.gd
@@ -15,6 +15,9 @@ func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_menu.tscn")
 
 
-
 func _on_imla_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/sambung ayat scenes/imla/imla_background.tscn")
+
+
+func _on_surah_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/sambung ayat scenes/surah/surah_background.tscn")

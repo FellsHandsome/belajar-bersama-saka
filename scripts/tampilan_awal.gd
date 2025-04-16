af1657764deeaabd852a28a3f67494e7fd297afa
LAB PPLG <prama0793@gmail.com>
2025-04-16 14:48:11 +0700

    Atur ulang soal and ganti background kuis

--- scripts/tampilan_awal.gd
+++ scripts/tampilan_awal.gd
@@ -1,11 +1,6 @@
 extends Control
 
-func _ready() -> void:
-	pass #
-
-func _process(delta: float) -> void:
-	pass #
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game_menu.tscn") # Replace with function body.
+	$CanvasLayer/Button/Click.play()
+	get_tree().change_scene_to_file("res://scenes/game_menu.tscn")
 	

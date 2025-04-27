2d52fdfdcf4c08fba1e2374c0865686e4b087c15
LAB PPLG <prama0793@gmail.com>
2025-04-27 12:59:28 +0700

    Menambahkan Sound Effect Button

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -1,19 +1,21 @@
 extends Control
 
 func _on_setting_button_pressed() -> void:
+	SoundManager.play_button_click_sound()
 	get_tree().change_scene_to_file("res://scenes/tampilan_setting.tscn") # Replace with function body.
 
 func _on_back_button_pressed() -> void:
+	SoundManager.play_button_click_sound()
 	get_tree().change_scene_to_file("res://scenes/tampilan_awal.tscn")
 
 func _on_tebak_gambar_pressed() -> void:
-	$"Tebak gambar/Click".play()
+	SoundManager.play_button_click_sound()
 	get_tree().change_scene_to_file("res://scenes/tebak gambar scenes/tebak_gambar_menu.tscn")
 
 func _on_sambung_ayat_pressed() -> void:
-	$"Sambung ayat/Click".play()
+	SoundManager.play_button_click_sound()
 	get_tree().change_scene_to_file("res://scenes/sambung ayat scenes/sambunng_ayat_menu.tscn")
 
 func _on_kuis_pressed() -> void:
-	$Kuis/Click.play()
+	SoundManager.play_button_click_sound()
 	get_tree().change_scene_to_file("res://scenes/kuis scenes/kuis_menu.tscn")
--- scripts/sound_manager.gd
+++ scripts/sound_manager.gd
@@ -1,17 +1,26 @@
-extends Control
+extends Node
 
 var backsound_player: AudioStreamPlayer
+var button_click_player: AudioStreamPlayer
 var is_muted := false
 
 func _ready():
-	var backsound_stream = load("res://assets/Asset game/Musik/middle-eastern-arabian-night-version-5-very-short-SBA-300504727-preview.ogg")
-	backsound_stream.loop = true
-
 	backsound_player = AudioStreamPlayer.new()
-	backsound_player.stream = backsound_stream
-	backsound_player.volume_db = 0
+	backsound_player.stream = load("res://assets/Asset game/Musik/middle-eastern-arabian-night-version-5-very-short-SBA-300504727-preview.ogg")
 	add_child(backsound_player)
 	backsound_player.play()
+
+	backsound_player.finished.connect(_on_backsound_finished)
+
+	button_click_player = AudioStreamPlayer.new()
+	add_child(button_click_player)
+
+func _on_backsound_finished():
+	backsound_player.play()
+	
+func play_backsound():
+	if not backsound_player.playing:
+		backsound_player.play()
 
 func mute():
 	is_muted = true
@@ -20,3 +29,7 @@ func mute():
 func unmute():
 	is_muted = false
 	backsound_player.volume_db = 0
+
+func play_button_click_sound():
+	button_click_player.stream = load("res://assets/Asset game/Musik/button-305770.wav")
+	button_click_player.play()
